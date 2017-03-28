2af8b1d17fa0ff2ec3c34bc495400f9e13105afd
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2017-03-28 18:30:01 +0200

    Spaghetti wrangle ProjectReport
    Reangranged the order of the init function, moved the save button action listener to it's own class and added comments/docstrings :smile:

--- src/LineCount/GUI/Windows/ProjectReport.java
+++ src/LineCount/GUI/Windows/ProjectReport.java
@@ -25,8 +25,13 @@ class ProjectReport extends JFrame{
     private CodeFile[] files;
 
     /**
-     * @param files Path array of paths for project files
-     * @param root Path object of the root directory of the project
+     * Constructor for the ProjectReport Frame (aka window)
+     * The constructor initiate the window and configures it as necessary,
+     * and then creates and loads a ReportPanel object containing all the GUI elements.
+     * @param files Path array of files to generate report upon
+     * @param root The root directory the files originate from
+     *             (Used to generate relative path)
+     * @param parsers ParserChooser loaded with Parsers to match the files against
      */
     ProjectReport(Path[] files, Path root, ParserChooser parsers){
         this.setTitle("Project Report");
@@ -41,64 +46,73 @@ class ProjectReport extends JFrame{
     }
 
 
+    /**
+     * JPanel containing all the GUI elements, and associated logic,
+     * for the ProjectReport window
+     */
     class ReportPanel extends JPanel{
-        FileTable fileOverview;
+        FileTable fileTable;
         private String[] saveOptions = {"Markdown", "Plaintext"};
-        private JComboBox saveChooser;
+        private JComboBox formatPicker;
 
+        /**
+         * Call the initializer method with a try with resources statement
+         * thus making sure not to leave any orphaned GUI elements.
+         */
         ReportPanel(){
             try {init();} catch (Exception e){
+                // TODO: Close the window if an error occurs
                 e.printStackTrace();
             }
         }
 
+        /**
+         * Initializes the GUI, creating and positioning the GUI objects
+         * @throws Exception If you try to generate a report from an empty File Array
+         */
         private void init() throws Exception{
             this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-            this.saveChooser = new JComboBox(saveOptions);
-            this.saveChooser.setMaximumSize( this.saveChooser.getPreferredSize() );
-            fileOverview = new FileTable(files);
+            // --------- Create the GUI objects ---------
+
+            // Big title so the user know's what it is
             JLabel title = new JLabel("Project report");
             title.setFont(new Font(title.getFont().getFontName(), Font.PLAIN, 16));
+            title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+            // Show root dir of the project
             JLabel pathLabel = new JLabel();
             if (files.length > 0) {
+                // Assuming all files are from the same root dir, so just get the path from the first
                 pathLabel.setText("Project path: " + files[0].getRootDir());
             } else {
                 pathLabel.setText("Please choose a file in the project manager");
             }
 
+            // Show the amount of files
             JLabel fileCountLabel = new JLabel("Number of files: " + files.length);
 
+            // Create a FileTable to display the files
             JLabel tableTitle = new JLabel("File overview");
-            title.setAlignmentX(Component.CENTER_ALIGNMENT);
-            fileOverview.setAlignmentX(Component.CENTER_ALIGNMENT);
+            fileTable = new FileTable(files);
+            fileTable.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+            // Save button
             JButton save = new JButton("Save");
-
-            save.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent actionEvent) {
-                    ReportFile reportFile = new ReportFile(files);
-                    try {
-                        if (saveChooser.getSelectedIndex() == 0){
-                            reportFile.saveMarkDownReport(Paths.get(files[0].getRootDir(), "ProjectReport.md"));
-                        } else {
-                            reportFile.saveTxtReport(Paths.get(files[0].getRootDir(), "ProjectReport.txt"));
-                        }
-
-                    } catch (IOException e){
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-            // Set alignment on the buttons
+            save.addActionListener(new saveListener());
             save.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            // Create a drop down where the user can choose export format
+            this.formatPicker = new JComboBox(saveOptions);
+            // We don't want the comboBox to stretch, it looks hideous
+            this.formatPicker.setMaximumSize( this.formatPicker.getPreferredSize() );
+
+            // Button box holding both buttons so they can be side by side
             Box buttonBox = Box.createHorizontalBox();
             buttonBox.add(save);
-            buttonBox.add(padX(this.saveChooser, 20));
+            buttonBox.add(padX(this.formatPicker, 20));
 
+            // ------- Add objects to parent panel ------
             this.add(Box.createRigidArea(new Dimension(0, 10)));
             this.add(title);
             this.add(Box.createRigidArea(new Dimension(0, 20)));
@@ -106,11 +120,29 @@ class ProjectReport extends JFrame{
             this.add(leftJustify(fileCountLabel, 10));
             this.add(Box.createRigidArea(new Dimension(0, 20)));
             this.add(leftJustify(tableTitle, 5));
-            this.add(fileOverview);
+            this.add(fileTable);
             this.add(buttonBox);
             this.add(Box.createRigidArea(new Dimension(0, 10)));
         }
 
+        /**
+         * Listen to the save button
+         */
+        class saveListener implements ActionListener{
+            public void actionPerformed(ActionEvent e){
+                ReportFile reportFile = new ReportFile(files);
+                try {
+                    if (formatPicker.getSelectedIndex() == 0){
+                        reportFile.saveMarkDownReport(Paths.get(files[0].getRootDir(), "ProjectReport.md"));
+                    } else {
+                        reportFile.saveTxtReport(Paths.get(files[0].getRootDir(), "ProjectReport.txt"));
+                    }
+
+                } catch (IOException exc){
+                    exc.printStackTrace();
+                }
+            }
+        }
     }
 
 }
